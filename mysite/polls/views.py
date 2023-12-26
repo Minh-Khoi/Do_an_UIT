6808bbcaae56142398affe67ccf8af03f7b44743
BUI TRUONG VI <49474873+Buitruongvi@users.noreply.github.com>
2023-12-26 22:50:30 +0700

    Commit save_home in views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -31,15 +31,54 @@ def thong_tin_hc(request):
     return render(request, CONST_ROOT.ROOT + '/mysite/templates_html/thongke.html')
 
 
-def save_home(request : HttpRequest):
-    request_body = request.body
-    datas = json.loads(request_body)
-    json_obj = json.loads(request_body)
-    # goi class DAO va luu data
-    print(request.method)
-    
-    return HttpResponse("") 
-    pass 
+def save_home(request):
+    try:
+        request_body = request.body
+        data = json.loads(request_body)
+
+        # Extract data for BenhNhan
+        benhnhan_data = data.get('tb_BenhNhan', [])[0]
+        ID = benhnhan_data.get('ID')
+        IDBenhNhan = benhnhan_data.get('IDBenhNhan')
+        HoTenBenhNhan = benhnhan_data.get('HoTenBenhNhan')
+        GioiTinh = benhnhan_data.get('GioiTinh')
+        NamSinh = benhnhan_data.get('NamSinh')
+        DiaChi = benhnhan_data.get('DiaChi')
+        SDT = benhnhan_data.get('SDT')
+        BHYT = benhnhan_data.get('BHYT')
+        Del = benhnhan_data.get('Del')
+
+
+        bn = BenhNhan(id=ID, idbenhnhan=IDBenhNhan, hotenbenhnhan=HoTenBenhNhan, gioitinh=GioiTinh,
+                            namsinh=NamSinh, diachi=DiaChi, sdt=SDT, bhyt=BHYT, dlt=Del)
+
+        PatientsDao().insert(bn)
+
+        sieuam_data = data.get('tb_SieuAm', [])[0]
+        ID_SieuAm = sieuam_data.get('ID')
+        Ngay = sieuam_data.get('Ngay')
+        SoPhieu = sieuam_data.get('SoPhieu')
+        IDBenhNhan_SieuAm = sieuam_data.get('IDBenhNhan')
+        TenBenhNhan_SieuAm = sieuam_data.get('TenBenhNhan')
+        TenBacSiSieuAm = sieuam_data.get('TenBacSiSieuAm')
+        TenBacSiChiDinh = sieuam_data.get('TenBacSiChiDinh')
+        MauSieuAm = sieuam_data.get('MauSieuAm')
+        ChanDoan = sieuam_data.get('ChanDoan')
+        NoiDung1 = sieuam_data.get('NoiDung1')
+        NoiDung2 = sieuam_data.get('NoiDung2')
+        KetLuan = sieuam_data.get('KetLuan')
+        DeNghi = sieuam_data.get('DeNghi')
+        Khoa = sieuam_data.get('Khoa')
+
+        sieuam = SieuAm(id=ID_SieuAm, idbenhnhan=IDBenhNhan_SieuAm, tenbenhnhan=TenBenhNhan_SieuAm, ngay=Ngay, sophieu=SoPhieu, tenbacsisieuam=TenBacSiSieuAm,
+                        tenbacsichidinh=TenBacSiChiDinh, mausieuam=MauSieuAm, chuandoan=ChanDoan, noidung1=NoiDung1,
+                        noidung2=NoiDung2, ketluan=KetLuan, denghi=DeNghi, khoa=Khoa)
+
+        UltrasoundDao().insert(sieuam)
+
+        return HttpResponse('Data saved successfully!')
+    except Exception as e:
+        return HttpResponse(f'Error: {str(e)}')
 
 def save_bacsi(request):
     request_body = request.body
